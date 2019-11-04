11554ce2e61a7dc6b16d1b09069eab7d01966b63
AmrH <AmrH@DEV-AMR>
2019-11-04 11:42:42 +0200

    Add project files.

---
+++ CollectionViewDemos/Views/Layout/MyPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CollectionViewDemos.ViewModels;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Views
+{
+    public partial class myPage : ContentPage
+    {
+        public myPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
+        }
+    }
+}
